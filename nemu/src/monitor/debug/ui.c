9d5bca5307b06902a996b878f0b0e69d3c995f50
tracer-ics2015 <tracer@njuics.org>
2015-11-02 14:34:19 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  14:34:19 up  2:25,  5 users,  load average: 1.11, 1.03, 0.94 b34318bed24d1d8fc4f98e8c539c1fd6461bb6

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -248,18 +248,18 @@ static int cmd_bt(char *args){
 	}
 
 	for(k=0; k<len; k++){
-		PartOfStackFrame s = func_stack[k];
+		//PartOfStackFrame s = func_stack[k];
 		if(k == 0){
 			for(i=0; i< nr_symtab_entry; i ++){
-			if(symtab[i].st_info == STT_FUNC && symtab[i].st_value <=cpu.eip && cpu.eip <= symtab[i].st_value + symtab[i]. st_size){
+			if(symtab[i].st_info == STT_FUNC ){
 				printf("STT_FUNC = %d, %d\n",STT_FUNC,  i);
-				break;
+				//break;
 			}
 		}
-			printf("#%d \tin %s \n",k, strtab+symtab[i].st_name);
+			//printf("#%d \tin %s \n",k, strtab+symtab[i].st_name);
 		}
-		else
-			printf("#%d \t0x%x in %s\n", k, s.ret_addr, strtab+symtab[i].st_name );
+		//else
+			//printf("#%d \t0x%x in %s\n", k, s.ret_addr, strtab+symtab[i].st_name );
 	}
 
 	return 0;
