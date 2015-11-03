74c8b2c43682e93c90ebba69f678105b8c6276fa
tracer-ics2015 <tracer@njuics.org>
2015-11-03 09:12:23 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  09:12:23 up  1:05,  2 users,  load average: 0.15, 0.10, 0.10 cde2ad13eb53fe46322b69a242969489e7347d

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -3,14 +3,27 @@
 #define instr setcc
 
 static void do_execute () {
-	switch(ops_decoded.opcode){
-		case 97:
-			if(CF == 0 && ZF == 0){
-				OPERAND_W(op_dest,1);
-			}else OPERAND_W(op_dest , 0);
-			break;
+	int condition = 0;
+	switch(ops_decoded.opcode & 0x011){
+		case 0x97: condition = (CF ==0 && ZF == 0);break;
+		case 0x93: condition = (CF == 0);break;
+		case 0x92: condition = (CF == 1); break;
+		case 0x96: condition = (CF == 1 || ZF == 1); break;
+		case 0x94: condition = (ZF == 1); break;
+		case 0x9f: condition = (ZF == 0 || SF == OF); break;
+		case 0x9d: condition =(SF ==OF);break;
+		case 0x9c: condition = (SF != OF); break;
+		case 0x9e: condition = (ZF == 1 && SF != OF); break;
+		case 0x95: condition = (ZF == 0); break;
+		case 0x91: condition = (OF ==0); break;
+		case 0x9b: condition = (PF == 0); break;
+		case 0x99: condition = (SF ==0 );break;
+		case 0x90: condition = (OF == 0); break;
+		case 0x9a: condition = (PF==1); break;
+		case 0x98: condition = (SF == 1); break;
 		default: break;
 	}
+	OPERAND_W(op_dest , condition);
 	/* TODO: Update EFLAGS. */
 
 	print_asm_template2();
