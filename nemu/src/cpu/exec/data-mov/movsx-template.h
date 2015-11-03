725fa33dc5e846a1144c57de6f476d1ee175a443
tracer-ics2015 <tracer@njuics.org>
2015-11-03 09:44:19 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  09:44:19 up  1:37,  2 users,  load average: 0.13, 0.17, 0.15 4750ab5ed53a9f63c11b097452a5e3b637e905b

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -2,12 +2,19 @@
 #define instr movsx
 
 static void do_execute (){
-	DATA_TYPE_S signExtend = 0;
 	if(DATA_BYTE == 1)
-		signExtend = (DATA_TYPE_S)((int8_t) op_src->val);
-	else if(DATA_BYTE == 2)
-		signExtend = (DATA_TYPE_S)((int16_t) op_src->val);
-	OPERAND_W(op_dest , signExtend);
+		if(op_dest->size == 2){
+			int16_t signExtend = (int16_t)((int8_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}else {
+			int32_t signExtend = (int32_t)((int8_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}
+	else if(DATA_BYTE == 2){
+		int32_t signExtend = (int32_t)((int16_t) op_src->val);
+		OPERAND_W(op_dest , signExtend);
+	}
+	
 	print_asm_template2();
 }
 
--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -7,7 +7,24 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//panic("please implement me");
+
+	unset_EFLAGS(E_CF);
+	unset_EFLAGS(E_OF);
+
+	if(result == 0) set_EFLAGS(E_ZF);
+	else unset_EFLAGS(E_ZF);
+
+	uint8_t lowByte = result & 0xff;
+	if((! (lowByte & 0x01)) ^ (! (lowByte & 0x02) )^ (! (lowByte & 0x04)) ^ (! (lowByte & 0x08) ) ^ (! (lowByte & 0x10)) 
+		^ (! (lowByte & 0x20) )^(! (lowByte & 0x40) )^ (! (lowByte & 0x80) )){
+		unset_EFLAGS(E_PF);	//偶数个0
+	}else set_EFLAGS(E_PF);
+
+	if(DATA_BYTE == 1 && (result & 0x80) ) set_EFLAGS(E_SF);
+	else if(DATA_BYTE == 2 && (result & 0x8000)) set_EFLAGS(E_SF);
+	else if(DATA_BYTE == 4 && (result & 0x80000000)) set_EFLAGS(E_SF);
+	else unset_EFLAGS(E_SF);
 
 	print_asm_template2();
 }
