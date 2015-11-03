72564484dbacf35c50a9d86117ada4943554c088
tracer-ics2015 <tracer@njuics.org>
2015-11-03 10:21:19 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  10:21:19 up  2:14,  2 users,  load average: 0.09, 0.08, 0.10 4521a110d56d5b5b5dd0a01a8a931746b454fdf2

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -6,9 +6,11 @@ static void do_execute (){
 	if(CF == 1 || ZF == 1){
 		if(DATA_BYTE == 1)
 			cpu.eip += (int32_t)((int8_t)op_src->val);
-		if(DATA_BYTE == 2){
+		else if(DATA_BYTE == 2){
 			cpu.eip += (int32_t)((int16_t)op_src->val);
 			cpu.eip = cpu.eip & 0x0000ffff;
+		}else{
+			cpu.eip +=(int32_t) op_src->val;
 		}
 	}
 	print_asm_template1();
