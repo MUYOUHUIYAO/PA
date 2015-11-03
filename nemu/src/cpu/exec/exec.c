183b16078910e10065e5ffe70e6dd62b72c1d0ad
tracer-ics2015 <tracer@njuics.org>
2015-11-03 16:21:45 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  16:21:45 up  1:25,  2 users,  load average: 0.16, 0.15, 0.17 e748d5d2967098299c6c00de52420218854be99e

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -222,7 +222,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0xe8 */	inv, inv, inv, inv,
 /* 0xec */	inv, inv, inv, inv,
 /* 0xf0 */	inv, inv, inv, inv,
-/* 0xf4 */	inv, inv, inv, inv,
+/* 0xf4 */	inv, inv, not_rm_b, not_rm_v,
 /* 0xf8 */	inv, inv, inv, inv,
 /* 0xfc */	inv, inv, inv, inv
 };
