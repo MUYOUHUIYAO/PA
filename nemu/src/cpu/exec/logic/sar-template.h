0296876cc850fb2d288b907457f414c9021b113c
tracer-ics2015 <tracer@njuics.org>
2015-11-03 12:48:41 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  12:48:41 up  4:42,  2 users,  load average: 0.22, 0.23, 0.17 8834f59aa4fb2f4728a99b99389da91c4c98a50c

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -7,13 +7,37 @@ static void do_execute () {
 	DATA_TYPE_S dest = op_dest->val;
 
 	uint8_t count = src & 0x1f;
-	dest >>= count;
+	uint8_t temp= count;
+	while(temp !=0){
+		if(dest & 0x01) set_EFLAGS(E_CF);
+		else unset_EFLAGS(E_CF);
+		dest /= 2;
+		temp -=1;
+	}
+
 	OPERAND_W(op_dest, dest);
 
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
 
+	 if(count == 1){
+		 unset_EFLAGS(E_OF);
+	 }
+
+	 if(dest == 0) set_EFLAGS(E_ZF);
+	 else unset_EFLAGS(E_ZF);
+
+	 uint8_t lowByte = dest & 0xff;
+	if((! (lowByte & 0x01)) ^ (! (lowByte & 0x02) )^ (! (lowByte & 0x04)) ^ (! (lowByte & 0x08) ) ^ (! (lowByte & 0x10)) 
+		^ (! (lowByte & 0x20) )^(! (lowByte & 0x40) )^ (! (lowByte & 0x80) )){
+		unset_EFLAGS(E_PF);	//偶数个0
+	}else set_EFLAGS(E_PF);
+
+	if(DATA_BYTE == 1 && (dest & 0x80) ) set_EFLAGS(E_SF);
+	else if(DATA_BYTE == 2 && (dest & 0x8000)) set_EFLAGS(E_SF);
+	else if(DATA_BYTE == 4 && (dest & 0x80000000)) set_EFLAGS(E_SF);
+	else unset_EFLAGS(E_SF);
 	print_asm_template2();
 }
 
