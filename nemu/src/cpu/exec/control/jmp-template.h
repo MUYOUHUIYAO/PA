281b23df42fbf8a99467803dff777a9a81fb915f
tracer-ics2015 <tracer@njuics.org>
2015-11-04 22:31:47 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  22:31:47 up  4:12,  2 users,  load average: 0.38, 0.27, 0.24 9484c277b9c55af03e6cd3e046faf1fe97549fd4

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,28 +3,28 @@
 
 
 static void do_execute (){
-	uint32_t op = ops_decoded.opcode;
-	if(op == 0xeb || op == 0xe9 ){
-		cpu.eip += op_src->val;
-		if(DATA_BYTE == 2){
-			cpu.eip = cpu.eip && 0x0000ffff ;
-		}
-	}else if(op == 0xff){
-		printf("%x\t%x\n",DATA_BYTE , op_src->val);
-		if(2 == DATA_BYTE){
-			cpu.eip = ( op_src->val)&0x0000ffff;
-		}
-		else if(4 == DATA_BYTE){
-			cpu.eip =  op_src->val;
-		}
+	cpu.eip += op_src->val;
+	if(DATA_BYTE == 2){
+		cpu.eip = cpu.eip && 0x0000ffff ;
 	}
+
 
 	print_asm_template1();
 }
 
 make_instr_helper(i)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(rm)
+
+make_helper(concat(jmp_rm_, SUFFIX)){
+	if(2 == DATA_BYTE){
+		cpu.eip = ( op_src->val)&0x0000ffff;
+	}
+	else if(4 == DATA_BYTE){
+		cpu.eip =  op_src->val;
+	}
+	return concat(decode_rm_, SUFFIX)(eip+1) + 1;
+	
+}
 #endif
 
 #include "cpu/exec/template-end.h"
