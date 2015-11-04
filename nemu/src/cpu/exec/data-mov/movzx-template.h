dd481ac7ecb0d151930d70aac9937cf817d17630
tracer-ics2015 <tracer@njuics.org>
2015-11-04 19:28:29 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  19:28:29 up  1:09,  2 users,  load average: 0.41, 0.28, 0.24 c71e3b6c53063fd7d0dfd74a5ee95f7ecd9975d4

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -2,12 +2,12 @@
 #define instr movzx
 
 static void do_execute (){
-	DATA_TYPE signExtend = 0;
+	uint32_t zeroExtend = 0;
 	if(DATA_BYTE == 1)
-		signExtend = (DATA_TYPE)((uint8_t) op_src->val);
+		zeroExtend = 0x000000ff & op_src->val;
 	else if(DATA_BYTE == 2)
-		signExtend = (DATA_TYPE)((uint16_t) op_src->val);
-	OPERAND_W(op_dest , signExtend);
+		zeroExtend = 0x0000ffff & op_src->val;
+	OPERAND_W(op_dest , zeroExtend);
 	print_asm_template2();
 }
 
