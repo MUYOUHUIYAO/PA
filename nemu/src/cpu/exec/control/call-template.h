aa228b729706d398f68568ab2f6b91cb1276355a
tracer-ics2015 <tracer@njuics.org>
2015-11-04 22:05:19 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  22:05:19 up  3:46,  2 users,  load average: 0.33, 0.22, 0.26 755afeb4b03f43e6c86a8c7f46f57326a844055d

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,21 +3,38 @@
 
 
 static void do_execute (){
-	if(2 == DATA_BYTE){
-		cpu.esp =cpu.esp - 2;
-		MEM_W(cpu.esp, (uint16_t)(cpu.eip& 0x0000ffff));
+	if(ops_decoded.opcode == 0xe8 ){
+		if(2 == DATA_BYTE){
+			cpu.esp =cpu.esp - 2;
+			MEM_W(cpu.esp, (uint16_t)(cpu.eip& 0x0000ffff));
 
-		cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
-	}
-	else if(4 == DATA_BYTE){
-		cpu.esp = cpu.esp-4;
-		MEM_W(cpu.esp,  cpu.eip);
-		cpu.eip = cpu.eip+ op_src->val;
+			cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
+		}
+		else if(4 == DATA_BYTE){
+			cpu.esp = cpu.esp-4;
+			MEM_W(cpu.esp,  cpu.eip);
+			cpu.eip = cpu.eip+ op_src->val;
+		}
+	}else if(ops_decoded.opcode == 0xff){
+		if(2 == DATA_BYTE){
+			cpu.esp =cpu.esp - 2;
+			MEM_W(cpu.esp, (uint16_t)(cpu.eip& 0x0000ffff));
+
+			cpu.eip = ( op_src->val)&0x0000ffff;
+		}
+		else if(4 == DATA_BYTE){
+			cpu.esp = cpu.esp-4;
+			MEM_W(cpu.esp,  cpu.eip);
+			cpu.eip =  op_src->val;
+		}
 	}
 
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+///#if DATA_BYTE == 2 || DATA_BYTE == 4
+//make_instr_helper(rm)
+//#endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,19 +3,25 @@
 
 
 static void do_execute (){
-	//uint32_t op = ops_decoded.opcode;
-	//if(op == 0xe8 || op == 0xe9 ){
+	uint32_t op = ops_decoded.opcode;
+	if(op == 0xe8 || op == 0xe9 ){
 		cpu.eip += op_src->val;
 		if(DATA_BYTE == 2){
 			cpu.eip = cpu.eip && 0x0000ffff ;
 		}
-	//}else if(op == 0xff){
-
-	//}
+	}else if(op == 0xff){
+		if(2 == DATA_BYTE){
+			cpu.eip = ( op_src->val)&0x0000ffff;
+		}
+		else if(4 == DATA_BYTE){
+			cpu.eip =  op_src->val;
+		}
+	}
 
 	print_asm_template1();
 }
 
 make_instr_helper(i)
+//make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
