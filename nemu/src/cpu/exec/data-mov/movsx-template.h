4baca7592fc7417077cfdf343071d5c3081c9de6
tracer-ics2015 <tracer@njuics.org>
2015-11-04 11:31:33 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  11:31:33 up  1:17,  2 users,  load average: 0.07, 0.13, 0.13 cef12085e83fd06f1c9c1c8b272063c58bcd1d55

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -2,7 +2,7 @@
 #define instr movsx
 
 static void do_execute (){
-	if(DATA_BYTE == 1){	printf("%d\n",op_dest->size );
+	/*if(DATA_BYTE == 1){	printf("%d\n",op_dest->size );
 		if(op_dest->size == 2){
 			int16_t signExtend = (int16_t)((int8_t) op_src->val);
 			OPERAND_W(op_dest , signExtend);
@@ -13,6 +13,19 @@ static void do_execute (){
 	}else if(DATA_BYTE == 2){
 		int32_t signExtend = (int32_t)((int16_t) op_src->val);
 		OPERAND_W(op_dest , signExtend);
+	}*/
+	printf("%d\t%d\t%d\n",DATA_BYTE,op_src->size,op_dest->size );
+	if(DATA_BYTE ==2){
+		int16_t signExtend = (int16_t)((int8_t) op_src->val);
+		OPERAND_W(op_dest , signExtend);
+	}else{
+		if(op_src->size == 1){
+			int32_t signExtend = (int32_t)((int16_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}else{
+			int32_t signExtend = (int32_t)((int8_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}
 	}
 	
 	print_asm_template2();
