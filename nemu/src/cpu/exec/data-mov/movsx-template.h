bf1cf105aa501bd3d365ef0f871c6ec70e6075e4
tracer-ics2015 <tracer@njuics.org>
2015-11-04 12:42:18 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  12:42:18 up  2:28,  2 users,  load average: 0.48, 0.20, 0.10 c57d81f089ee11768b25035b99f76eff364d2e4f

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -7,7 +7,7 @@ static void do_execute (){
 		int16_t signExtend = (int16_t)((int8_t) op_src->val);
 		OPERAND_W(op_dest , signExtend);
 	}else{
-		if(ops_decoded.opcode == (0xbe | 0x100)){
+		if(ops_decoded.opcode == (0xbf | 0x100)){
 			int32_t signExtend = (int32_t)((int16_t) op_src->val);
 			OPERAND_W(op_dest , signExtend);
 		}else{
