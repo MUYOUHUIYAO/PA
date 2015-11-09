c1e55af3aa60727bad2c6e3e7fe667dd130c8e42
tracer-ics2015 <tracer@njuics.org>
2015-11-09 21:52:29 +0800

    >  run 141220000 mhy Linux mhy-CW65S 3.13.0-67-generic #110-Ubuntu SMP Fri Oct 23 13:25:18 UTC 2015 i686 i686 i686 GNU/Linux  21:52:29 up  8:27,  2 users,  load average: 0.57, 0.59, 0.65 580e32c7a4695ce5a10d2a0259dde4753f022396

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,7 +1,12 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	int result = a * b / (1<<16);
+	int q = (1<<16);
+	int qa = a / q;
+	int qb = b /q;
+	int ya = a %q;
+	int yb = b % q;
+	int result = qa*qb *q + qa * yb +qb*ya + ya * yb/q;
 	return result;
 }
 
@@ -16,8 +21,11 @@ FLOAT f2F(float a) {
 	int sign = args & 0x80000000;
 	int e = ((args & 0x7f800000)>>23) - 127;
 	int m= (args & 0x007fffff) + 0x00800000;
-	if(e > 7)  return sign | (m << (e -7));
-	else return sign | (m >> (7 -e));
+	int result;
+	if(e > 7)  result =(m << (e -7));
+	else result = (m >> (7 -e));
+	if(sign) result = -result;
+	return result;
 }
 
 FLOAT Fabs(FLOAT a) {
