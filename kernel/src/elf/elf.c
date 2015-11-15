6d9ba51a4dc3449642dd3d06efe1073666a0f8b4
tracer-ics2015 <tracer@njuics.org>
2015-11-15 17:57:24 +0800

    >  compile kernel 141220000 mhy Linux mhy-CW65S 3.13.0-69-generic #112-Ubuntu SMP Tue Nov 10 16:30:03 UTC 2015 i686 i686 i686 GNU/Linux  17:57:24 up  5:01,  5 users,  load average: 1.15, 0.97, 0.89 cc4a8004f7f29c2a55932e08b55af512226f14b4

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -60,12 +60,12 @@ uint32_t loader() {
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
 
-			 memcpy(pa_to_va(ph -> p_vaddr) , (void*)buf + ph -> p_vaddr, ph -> p_filesz);
+			 memcpy((void*)(0x800000+ph -> p_vaddr) , (void*)buf + ph -> p_vaddr, ph -> p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			 memset(pa_to_va(ph -> p_vaddr+ ph -> p_filesz), 0, ph -> p_memsz);
+			 memset((void*)(0x800000+ph -> p_vaddr+ ph -> p_filesz), 0, ph -> p_memsz);
 
 
 #ifdef IA32_PAGE
