5fb43ac4db224883d6e47bf443b8200e3c7295e1
tracer-ics2015 <tracer@njuics.org>
2015-11-15 18:21:28 +0800

    >  compile kernel 141220000 mhy Linux mhy-CW65S 3.13.0-69-generic #112-Ubuntu SMP Tue Nov 10 16:30:03 UTC 2015 i686 i686 i686 GNU/Linux  18:21:28 up  5:25,  4 users,  load average: 1.06, 1.08, 0.99 a3502d0ec3da6e7349be37f1862a87bd384be90f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -59,7 +59,7 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 nemu_assert(i ==1 && ph -> p_filesz ==0x00000);
+			 nemu_assert(i ==1 && ph -> p_vaddr ==0x008011c0);
 			 ramdisk_read((uint8_t*)(ph -> p_vaddr), ph -> p_offset, ph -> p_filesz);
 			 //memcpy((void*)(ph -> p_vaddr) , (void*)buf + ph -> p_vaddr, ph -> p_filesz);
 			 
