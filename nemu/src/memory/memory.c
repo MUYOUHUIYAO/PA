b3048be0ea3028cae3a6bde3c8665ddc8d1026ee
tracer-ics2015 <tracer@njuics.org>
2016-01-25 15:06:52 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-69-generic #112-Ubuntu SMP Tue Nov 10 16:30:03 UTC 2015 i686 i686 i686 GNU/Linux  15:06:52 up  1:37,  3 users,  load average: 0.85, 0.87, 0.61 7e10b96a9aa5e70baa1082c0e075306e31334314

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -17,7 +17,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 		++i;
 		--len;
 	}
-	printf("wrong in hread addr = 0x%x\tlen = %d\tdata=0x%x\n", addr, l,result);
+	//printf("wrong in hread addr = 0x%x\tlen = %d\tdata=0x%x\n", addr, l,result);
 	return result & (~0u >> ((4 - l) << 3));
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
@@ -26,7 +26,7 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	//printf("hwrite_error!!!\n");
 
 	int i = 0;
-	printf("hwrite addr = 0x%x len = %d data=0x%x\n",addr, len, data );
+	//printf("hwrite addr = 0x%x len = %d data=0x%x\n",addr, len, data );
 	while(len){
 		CacheWriteByte(addr + i, (uint8_t)(data>>(8 * i)));
 		++i;
