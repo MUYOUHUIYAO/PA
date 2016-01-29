7b1a695ffed138e7e120989140b79d09b59ff913
tracer-ics2015 <tracer@njuics.org>
2016-01-29 11:42:55 +0800

    >  compile NEMU 141220000 mhy Linux mhy-CW65S 3.13.0-69-generic #112-Ubuntu SMP Tue Nov 10 16:30:03 UTC 2015 i686 i686 i686 GNU/Linux  11:42:55 up  1:55,  3 users,  load average: 0.16, 0.28, 0.26 22ec6b69aa1a497db96d4cc49da6aa158a2ce61d

--- nemu/src/memory/segment.c
+++ nemu/src/memory/segment.c
@@ -32,11 +32,13 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, SELECTOR selector){
 
 	r.low = lnaddr_read(segdesc_addr + 8 * index, 4);
 	r.high = lnaddr_read(segdesc_addr + 8 * index + 4, 4);
-	printf("0x%x, 0x%x\n", r.low, r.high );
+
+	segdesc = (SegDesc *)(&r);
 
 	uint32_t base = (segdesc -> base_31_24 << 24)| (segdesc -> base_23_16 << 16) | (segdesc -> base_15_0);
 	uint32_t limit = ((segdesc -> limit_19_16 << 16) | (segdesc -> limit_15_0)) & 0xfffff;
-	
+		printf("0x%x, 0x%x\n", base, limit );
+
 	Assert( addr + len < limit, "segment out limit, 0x%x, %d, 0x%x", addr, len, limit);	
 	return base + addr;
 }
